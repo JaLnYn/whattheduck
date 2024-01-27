a1f88b052c222c45c6e01cbcb9295ca32a8e5d4e
Alan Yuan <alan.yuan.jly@gmail.com>
2024-01-27 17:35:32 -0500

    some changes

--- whattheduck/src/App.js
+++ whattheduck/src/App.js
@@ -1,39 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import whatTheDuck from './whattheduck.svg'; 
-import favoritegoose from './favoritegoose.svg'; 
-import matching from './matching.svg'; 
-import chatting from './chatting.svg'; 
-import appearance from './appearance.svg'; 
-import gentlegoose from './gentlegoose.svg'; 
-import yesorno from './yesorno.svg'; 
-import yes from './yes.svg'; 
-import no from './no.svg'; 
-import rolodexbody from './rolodexbody.svg';
-import rolodexwheel from './rolodexwheel.svg'; 
-import welcome from './welcome.svg'; 
-import notepad from './notepad.svg';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import whatTheDuck from "./whattheduck.svg";
+import favoritegoose from "./favoritegoose.svg";
+import matching from "./matching.svg";
+import chatting from "./chatting.svg";
+import appearance from "./appearance.svg";
+import gentlegoose from "./gentlegoose.svg";
+import yesorno from "./yesorno.svg";
+import yes from "./yes.svg";
+import no from "./no.svg";
+import rolodexbody from "./rolodexbody.svg";
+import rolodexwheel from "./rolodexwheel.svg";
+import welcome from "./welcome.svg";
+import notepad from "./notepad.svg";
 
-function FirstPage({ isPopping, onImageClick }) {
+function Splash({ isPopping, onImageClick }) {
   return (
     <div className="Page" onClick={onImageClick}>
       <img
         src={whatTheDuck}
         alt="What the Duck"
-        className={isPopping ? 'pop-animation' : ''}
-        style={{ cursor: 'pointer' }}
+        className={isPopping ? "pop-animation" : ""}
+        style={{ cursor: "pointer" }}
       />
     </div>
   );
 }
 
-
-function ThirdPage() {
+function Notepad({ setMyBio, onButtonClick }) {
   return (
     <div className="Page Container">
       <div className="title">
         <img src={welcome} alt="welcome" className="welcome" />
       </div>
+
+      {/* temperature */}
+      <div>
+        <input
+          type="text"
+          value={tempText}
+          onChange={(e) => setTempText(e.target.value)}
+          placeholder="Enter some text"
+        />
+        <button onClick={onButtonClick}>continnue</button>
+      </div>
+      {/* //temp */}
 
       <div className="notepad-wrapper">
         <div className="notepad">
@@ -44,26 +55,18 @@ function ThirdPage() {
   );
 }
 
-
-function SecondPage() {
+function GooseSelector() {
   return (
     <div className="Page Container">
       <div className="title">
         <img src={favoritegoose} alt="Goose" className="favoritegoose" />
       </div>
-      
+
       <div className="photo">
         <div className="rolodex">
-          <div className="above">
-            <img src={gentlegoose} alt="gentlegoose" className="pagebackground2" />
-            <div className="info">
-              <div className="pic"></div>
-              <div className="bio">"This would be the description about the user"</div>
-            </div>
-          </div>
-
-          <div className="yesorno" >
-            <img src={yesorno} className="pagebackground"/>
+          <img src={gentlegoose} alt="gentlegoose" className="gentlegoose" />
+          <div className="yesorno">
+            <img src={yesorno} className="pagebackground" />
             <div className="buttons">
               <img src={yes} alt="yes" className="yes" />
               <img src={no} alt="no" />
@@ -74,7 +77,7 @@ function SecondPage() {
           <img src={rolodexwheel} alt="rolodexwheel" className="rolodexwheel" />
         </div>
       </div>
-      
+
       <div className="Menu">
         <img src={matching} alt="matching" className="matching" />
         <img src={chatting} alt="chatting" className="chatting" />
@@ -84,11 +87,11 @@ function SecondPage() {
   );
 }
 
-
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [isPopping, setIsPopping] = useState(false);
-  const [currentPage, setCurrentPage] = useState('first');
+  const [myBio, setMyBio] = useState("");
+  const [currentPage, setCurrentPage] = useState("first");
 
   useEffect(() => {
     setTimeout(() => {
@@ -99,24 +102,27 @@ function App() {
   }, []);
 
   function handleImageClick() {
-    setCurrentPage('second'); // Change the state to show the second page
+    setCurrentPage("second"); // Change the state to show the second page
   }
 
   let content;
   switch (currentPage) {
-    case 'first':
+    case "first":
       content = isLoading ? (
-        <div className="LoadingScreen"><p>Loading...</p></div>
+        <div className="LoadingScreen">
+          <p>Loading...</p>
+        </div>
       ) : (
-        <FirstPage isPopping={isPopping} onImageClick={handleImageClick} />
+        <Splash isPopping={isPopping} onImageClick={handleImageClick} />
       );
       break;
-    case 'second':
-      content = <SecondPage />;
+    case "second":
+      content = <Notepad />;
       break;
-    case 'third':
-      content = <ThirdPage />;
+    case "third":
+      content = <GooseSelector />;
       break;
+
     default:
       content = <div>Page not found</div>;
   }
